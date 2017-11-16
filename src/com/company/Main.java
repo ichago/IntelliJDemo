ec74251fca02eb0ef88df94a832a951c5bb42161
ichago <bleach-567@hotmail.fr>
2017-11-16 21:33:43 +0000

    Add a string variable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        String s = "bye";
         System.out.println("Hello World!");
+        System.out.println(s);
     }
 }
